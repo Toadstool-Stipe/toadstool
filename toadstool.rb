e07ae3cbf0c9a3ff77ca961f1f3a692a40b8e40f
Jed Foster <jed@jedfoster.com>
2012-04-26 08:22:47 -0700

    Point the Sass route at the sass dir, not the view dir.

--- toadstool.rb
+++ toadstool.rb
@@ -16,7 +16,7 @@ end
 # at a minimum, the main sass file must reside within the ./views directory. here, we create a ./views/stylesheets directory where all of the sass files can safely reside.
 get '/stylesheets/:name.css' do
   content_type 'text/css', :charset => 'utf-8'
-  scss(:"stylesheets/#{params[:name]}", Compass.sass_engine_options )
+  scss(:"../sass/#{params[:name]}", Compass.sass_engine_options )
 end
 
 
